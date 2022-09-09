4da8a9d59ccdcbd376d96e08319e0b5f0f893ffc
Jian Tay <jian.tay@colorado.edu>
2022-09-09 13:37:57 -0600

    Completed prototype
    * Modified block index code to include a buffer to allow for displacement of each block
    * Added code to generate a video to validate the results

--- getBlockIdxs.m
+++ getBlockIdxs.m
@@ -1,4 +1,4 @@
-function [rowIdxs, colIdxs] = getBlockIdxs(imgSize, blockSize)
+function [rowIdxs, colIdxs] = getBlockIdxs(imgSize, blockSize, varargin)
 %GETBLOCKIDXS  Divide an image into blocks
 %
 %  [R, C] = GETBLOCKIDXS(IMGSIZE, BLOCKSIZE) returns row and column indices
@@ -8,5 +8,11 @@ function [rowIdxs, colIdxs] = getBlockIdxs(imgSize, blockSize)
 %  specifying the [rows cols] of the image to be divided. BLOCKSIZE should
 %  be a 2x1 vector specifying the [HEIGHT WIDTH] of the resulting blocks.
 
-rowIdxs = 1:blockSize(1):imgSize(1);
-colIdxs = 1:blockSize(1):imgSize(2);
+if ~isempty(varargin)
+    numPixelBuffer = varargin{1};
+else
+    numPixelBuffer = 0;
+end
+
+rowIdxs = (1 + numPixelBuffer):blockSize(1):(imgSize(1) - numPixelBuffer);
+colIdxs = (1 + numPixelBuffer):blockSize(2):(imgSize(2) - numPixelBuffer);
--- testScript.m
+++ testScript.m
@@ -1,107 +1,124 @@
 clearvars
 clc
 
-file = 'D:\Projects\Research\2022-optical-flow-analyzer\data\wnt\wnt_12345_1_001.nd2';
+%file = 'D:\Projects\Research\2022-optical-flow-analyzer\data\wnt\wnt_12345_1_001.nd2';
+file = 'D:\Projects\Research\2022-optical-flow-analyzer\data\mature\mat_12345_1_001.nd2';
 
 reader = BioformatsImage(file);
 
 Iref = getPlane(reader, 1, 1, 1);
-Imoved = getPlane(reader, 1, 1, 2);
 
-[R, C] = getBlockIdxs(size(Iref), [16 16]);
+maxDisplacement = 7;
+[R, C] = getBlockIdxs(size(Iref), [32 32], maxDisplacement);
 
 %%
 
-%Initialize the output displacement matrix
-storeDisplacement = zeros(numel(R) - 1, numel(C) - 1);
+vid = VideoWriter('D:\Projects\Research\2022-optical-flow-analyzer\processed\mat_12345_1_001.avi');
+vid.FrameRate = 5;
+open(vid)
 
-maxDisplacement = 7;
-dispVector = -maxDisplacement:maxDisplacement;
+for iT = 1:reader.sizeT
 
-for iRow = 1
-    for iCol = 1
+    Imoved = getPlane(reader, 1, 1, iT);
 
-        currTemplate = Imoved(R(iRow):(R(iRow + 1) - 1), C(iCol):(C(iCol + 1) - 1));
+    %Initialize the output displacement matrix
+    storeDisplacement = cell(numel(R) - 1, numel(C) - 1);
 
-        %Initialize output matrix
-        errScore = zeros(2 * maxDisplacement + 1);
+    dispVector = -maxDisplacement:maxDisplacement;
 
-        for dRow = 1:numel(dispVector)
-            for dCol = 1:numel(dispVector)
+    %Create output image
+    figure(99)
+    imshow(Imoved, [])
+    hold on
 
-                refRowStart = R(iRow) + dispVector(dRow);
-                refRowEnd = R(iRow + 1) + dispVector(dRow) - 1;
+    for iRow = 1:(numel(R) - 1)
+        for iCol = 1:(numel(C) - 1)
 
-                if refRowStart < 1
-                    
-                    %Missing portion is 
-                    diffRow = abs(refRowStart) + 1;
-                    
-                    %Crop both reference and templates
-                    refRowStart = 1;
-                    refRowEnd = refRowEnd;
+            currTemplate = Imoved(R(iRow):(R(iRow + 1) - 1), C(iCol):(C(iCol + 1) - 1));
 
+            %Initialize output matrix
+            errScore = zeros(2 * maxDisplacement + 1);
+
+            for dRow = 1:numel(dispVector)
+                for dCol = 1:numel(dispVector)
+
+                    refRowStart = R(iRow) + dispVector(dRow);
+                    refRowEnd = R(iRow + 1) + dispVector(dRow) - 1;
+
+                    %                 if refRowStart < 1
+                    %
+                    %                     %Missing portion is
+                    %                     diffRow = abs(refRowStart) + 1;
+                    %
+                    %                     %Crop both reference and templates
+                    %                     refRowStart = 1;
+                    %                     refRowEnd = refRowEnd;
+                    %
+                    %                 end
+
+                    refColStart = C(iCol) + dispVector(dCol);
+                    refColEnd = C(iCol + 1) + dispVector(dCol) - 1;
+
+                    %                 if refColStart < 1
+                    %
+                    %                     diffCol = abs(refColStart) + 1;
+                    %
+                    %                     refColStart = 1;
+                    %                     refColEnd = refColEnd;
+                    %
+                    %                 end
+
+
+                    %Crop the reference image to the right section
+                    currRefSection = Iref(...
+                        refRowStart:refRowEnd, ...
+                        refColStart:refColEnd);
+
+                    %                 keyboard
+
+                    %Compute the MAD score
+                    errScore(dRow, dCol) = immse(currRefSection, currTemplate);
                 end
-
-                refColStart = C(iCol) + dispVector(dCol);
-                refColEnd = C(iCol + 1) + dispVector(dCol) - 1;
-
-                if refColStart < 1
-
-                    diffCol = abs(refColStart) + 1;
-
-                    refColStart = 1;
-                    refColEnd = refColEnd;
-
-                end
-
-
-                %Crop the reference image to the right section
-                currRefSection = Iref(...
-                    refRowStart:refRowEnd, ...
-                    refColStart:refColEnd);
-
-                keyboard
-
-                %Compute the MAD score
-                errScore(dRow, dCol) = immse(currRefSection, currTemplate);
             end
+
+            %Find the direction and magnitude of displacement
+            [maxScore, maxIdx] = min(errScore, [], 'all');
+
+            [I, J] = ind2sub([numel(dispVector), numel(dispVector)], maxIdx);
+
+            storeDisplacement{iRow, iCol} = [dispVector(I), dispVector(J)];
+
+            quiver(mean([C(iCol),(C(iCol + 1) - 1)]),...
+                mean([R(iRow),(R(iRow + 1) - 1)]), ...
+                dispVector(J), dispVector(I), 8);
+
         end
-
-        %Find the direction and magnitude of displacement
-        [maxScore, maxIdx] = max(errScore, [], 'all');
-
-        [I, J] = ind2sub([numel(dispVector), numel(dispVector)], maxIdx);
-
-        storeDisplacement(iRow, iCol) = [I, J];
-
     end
+    hold off
+    %Make a quiver plot
+    writeVideo(vid, getframe(gcf))
+
+
 end
 
+close(vid)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-        subplot(1, 2, 1)
-        imshow(Iref, [])
-        subplot(1, 2, 2)
-        imshow(currTemplate, [])
-
-
-
+%
+%
+%
+%
+%
+%         subplot(1, 2, 1)
+%         imshow(Iref, [])
+%         subplot(1, 2, 2)
+%         imshow(currTemplate, [])
+%
+%
+%
 
 
 
